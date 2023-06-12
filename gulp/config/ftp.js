96992ab66b9b5799218a5034b0465289b9e7a068
Dmytro Adamovych <ospulyny.it@gmail.com>
2023-06-12 18:58:11 +0300

    Start project

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+	host: "109.94.209.117",
+	user: "webmalik",
+	password: "odino4ka_9",
+	parallel: 5
+}
---
+++ gulp/tasks/js.js
@@ -0,0 +1,18 @@
+import webpackStream from "webpack-stream";
+
+export const js = () => {
+	return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
+		.pipe(app.plugins.plumber(
+			app.plugins.notify.onError({
+				title: "JS",
+				message: "Error <%= error.message %>"
+			})))
+		.pipe(webpackStream({
+			mode: app.isBuild ? 'production': 'development',
+			output: {
+				filename: 'app.min.js'
+			}
+		}))
+		.pipe(app.gulp.dest(app.path.build.js))
+		.pipe(app.plugins.browserSync.stream());
+}
